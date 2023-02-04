87c7b05c6be8e308c2d4cc4f6c2ca4819bb8828a
robingupta14 <rg721@ic.ac.uk>
2023-02-04 18:26:45 +0000

    final db file.

--- src/DataAnalysis.java
+++ src/DataAnalysis.java
@@ -1,16 +1,18 @@
 import java.util.HashMap;
 
 public class DataAnalysis {
-    int maxHumid = 100;
-    int maxTemp = 40;
-    int maxSalt = 1000;
+    double maxHumid = 77.5;
+    int maxTemp = 30;
+    int maxSalt = 450;
 
     public static class PlantRec {
+        String plantName;
         double idealTemperature; /* Normalised from 40 deg or lower */
         double humidity; /* normalised from 0 to 100 percent */
         double salinity; /* normalised from 0 to 1000 mg per liter */
 
-        public PlantRec(int t, int h, int s) {
+        public PlantRec(String p, double t, double h, double s) {
+            this.plantName = p;
             this.idealTemperature = t;
             this.humidity = h;
             this.salinity = s;
@@ -20,7 +22,17 @@ public class DataAnalysis {
     public HashMap<String, PlantRec> database = new HashMap<String, PlantRec>();
 
     public DataAnalysis() {
-        database.put("Oranges", new DataAnalysis.PlantRec(18, 45,350));
+        database.put("Oranges", new DataAnalysis.PlantRec("Oranges", 18, 45,350));
+        database.put("Corn", new DataAnalysis.PlantRec("Corn", 24, 65,250));
+        database.put("Potatoes", new DataAnalysis.PlantRec("Potatoes", 12.5, 85,250));
+        database.put("Wheat", new DataAnalysis.PlantRec("Wheat", 15, 65,450));
+        database.put("Rice", new DataAnalysis.PlantRec("Rice", 30, 75,350));
+        database.put("Maize", new DataAnalysis.PlantRec("Maize",12, 60,200));
+        database.put("Soybeans", new DataAnalysis.PlantRec("Soybeans",21, 72.5,150));
+        database.put("Tomatoes", new DataAnalysis.PlantRec("Tomatoes",22, 77.5,300));
+        database.put("Sugarcane", new DataAnalysis.PlantRec("Sugarcane",30, 60,100));
+        database.put("Grapes", new DataAnalysis.PlantRec("Grapes",16, 65,200));
+
     }
 
     public PlantRec plantIndex(int temperature, int humidity, int salinity) {
